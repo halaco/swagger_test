cd180d9243efceaeb2e427c82bd22c8b635f4b99
Sadayuki Kato <sadayuki.kato@gmail.com>
2016-02-10 02:19:20 +0000

    insert grape-entity definition

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 
 gem 'grape'
+gem 'grape-entity'
 gem 'grape-swagger'
 gem 'grape-swagger-rails'
 
--- app/api/add_api.rb
+++ app/api/add_api.rb
@@ -1,16 +1,25 @@
+module Entities
+    class Result < Grape::Entity
+        expose :value, as: 'sum'
+        expose :first
+        expose :last
+    end
+end
+
 class AddApi < Grape::API
     format :json
         
     resource :add do
             
-        desc "Add Two Values."
+        desc "Add Two Values.", entity:  Entities::Result
         params do
             requires :first, type: Integer, desc: "First Value."
             requires :second, type: Integer, desc: "Second Value."
         end
         get '' do
             sum = params[:first] + params[:second]
-            { value: sum, first: params[:first], second: params[:second]}
+            result = { value: sum, first: params[:first], second: params[:second]}
+            present result, with: Entities::Result
         end
     end
     
